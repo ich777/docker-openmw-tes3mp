4c47d76bc0aac08e32b0fb9def0b73c5f05615f6
ich777 <ich777@noreply.example.org>
2021-04-05 08:31:43 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -48,7 +48,7 @@ elif [ "$CUR_V" != "$GAME_V" ]; then
     fi
     cd ${DATA_DIR}/TES3MP-Server
     mkdir /tmp/backup
-    cp *.cfg /tmp/backup/
+    cp -R *.cfg CoreScripts/ resources/ /tmp/backup/
     rm -rf openmwtes3mp-*
     if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/openmw-tes3mp-v$GAME_V.tar.gz "$DL_URL" ; then
         echo "---Successfully downloaded OpenMW-TES3MP v$GAME_V---"
@@ -59,7 +59,7 @@ elif [ "$CUR_V" != "$GAME_V" ]; then
     mkdir -p ${DATA_DIR}/TES3MP-Server
     tar -C ${DATA_DIR}/TES3MP-Server --strip-components=1 -xf ${DATA_DIR}/openmw-tes3mp-v$GAME_V.tar.gz
 	touch ${DATA_DIR}/openmwtes3mp-$GAME_V
-    cp /tmp/backup/* ${DATA_DIR}/TES3MP-Server/
+    cp -R /tmp/backup/* ${DATA_DIR}/TES3MP-Server/
     rm ${DATA_DIR}/openmw-tes3mp-v$GAME_V.tar.gz /tmp/backup
 elif [ "$CUR_V" == "$GAME_V" ]; then
     echo "---OpenMW-TES3MP v$GAME_V up-to-date---"
@@ -69,6 +69,11 @@ echo "---Preparing Server---"
 if [ "$(cat ${DATA_DIR}/TES3MP-Server/tes3mp-server-default.cfg | grep "hostname = My TES3MP server")" ]; then
     sed -i '/hostname = My TES3MP server/c\hostname = TES3MP Docker Server\' ${DATA_DIR}/TES3MP-Server/tes3mp-server-default.cfg
     sed -i '/password =/c\password = Docker\' ${DATA_DIR}/TES3MP-Server/tes3mp-server-default.cfg
+fi
+if [ "$(cat ${DATA_DIR}/TES3MP-Server/CoreScripts/data/requiredDataFiles.json | grep '    {"Morrowind.esm": \["0x7B6AF5B9", "0x34282D67"\]},')" ]; then
+    sed -i '/    {"Morrowind.esm": \["0x7B6AF5B9", "0x34282D67"\]},/c\    {"Morrowind.esm": ["0x7B6AF5B9", "0x34282D67", "0x5F9766E6"]},\' ${DATA_DIR}/TES3MP-Server/CoreScripts/data/requiredDataFiles.json
+    sed -i '/    {"Tribunal.esm": \["0xF481F334", "0x211329EF"\]},/c\    {"Tribunal.esm": ["0xF481F334", "0x211329EF", "0x7C19567D"]},\' ${DATA_DIR}/TES3MP-Server/CoreScripts/data/requiredDataFiles.json
+    sed -i '/    {"Bloodmoon.esm": \["0x43DD2132", "0x9EB62F26"\]}/c\    {"Bloodmoon.esm": ["0x43DD2132", "0x9EB62F26", "0xF6FC8BE4"]}\' ${DATA_DIR}/TES3MP-Server/CoreScripts/data/requiredDataFiles.json
 fi
 chmod -R ${DATA_PERM} ${DATA_DIR}
 
